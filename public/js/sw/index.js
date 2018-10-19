430ab49f3410cec33a8193afce54fbeb4da4dbf6
Mustafa Khamisi <mustafangure@gmail.com>
2018-10-19 16:09:22 +0300

    feat: Return a html cached response

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -2,5 +2,9 @@ self.addEventListener('fetch', function(event) {
 	// TODO: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
 	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
+	event.respondWith(
+		new Response('Hello <strong class="a-winner-is-me">Mustafa</strong>', {
+			headers: {'Content-Type':'text/html'}
+		})
+	);
 });
